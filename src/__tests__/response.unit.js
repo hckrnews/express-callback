dc5296acbfed47bc10ec400dde2cd4a9e220f43d
Pieter Wigboldus <w3n@w3news.org>
2021-04-12 21:11:01 +0200

    Test the header overwrite

--- src/__tests__/response.unit.js
+++ src/__tests__/response.unit.js
@@ -17,7 +17,7 @@ const TestCases = [
         },
     },
     {
-        description: 'Test a simple request',
+        description: 'It should add the extra header',
         params: {
             statusCode: 201,
             headers: {
@@ -31,6 +31,31 @@ const TestCases = [
             headers: {
                 'Content-Type': 'application/json',
                 'Cache-Control': 'no-store, max-age=0',
+                test: 'ok',
+            },
+            statusCode: 201,
+            body: {
+                example: 42,
+            },
+        },
+    },
+    {
+        description: 'It should overwrite the default headers',
+        params: {
+            statusCode: 201,
+            headers: {
+                'Content-Type': 'text/html',
+                'Cache-Control': 'private',
+                test: 'ok',
+            },
+            body: {
+                example: 42,
+            },
+        },
+        expectedResult: {
+            headers: {
+                'Content-Type': 'text/html',
+                'Cache-Control': 'private',
                 test: 'ok',
             },
             statusCode: 201,
